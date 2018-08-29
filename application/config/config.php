51294c1dec5794f2839a9a185296fb0227c2a3be
Chacon <jaimechaconarevalo@gmail.com>
2018-08-29 16:15:29 -0300

    commit configuracion de base de datos y sitio web

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/gestion_calidad';
+$config['base_url'] = 'http://www.2gdc.com';
 
 /*
 |--------------------------------------------------------------------------
