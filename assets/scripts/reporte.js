46b61e1e52f6a3511d2c12a6039fea409ae5a677
Chacon <jaimechaconarevalo@gmail.com>
2019-01-02 16:44:47 -0300

    se modifican las tablas y filtros de listarReportes

--- assets/scripts/reporte.js
+++ assets/scripts/reporte.js
@@ -156,19 +156,19 @@
 		            row = row.concat('<tr>');
 		            if(data[i]['nombre'] == 'Total')
 		            {
-		            	row = row.concat('\n<th class="table-active">',data[i]['nombre'],'</th>');
-			            row = row.concat('\n<th class="text-center table-active">','----','</th>');
-			            row = row.concat('\n<th class="text-right table-active">$ ',formatNumber(data[i]['Recaudado_2018']),'</th>');
-			            row = row.concat('\n<th class="text-center table-active">','----','</th>');
-			            row = row.concat('\n<th class="text-right table-active">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</th>');
-			            row = row.concat('\n<th class="text-center table-active">',data[i]['var_18_17'],'</th>');
+		            	row = row.concat('\n<th class="table-active"><p class="texto-pequenio">',data[i]['nombre'],'</p></th>');
+			            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">','----','</p></th>');
+			            row = row.concat('\n<th class="text-right table-active"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2018']),'</p></th>');
+			            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">','----','</p></th>');
+			            row = row.concat('\n<th class="text-right table-active"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</p></th>');
+			            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">',data[i]['var_18_17'],'</p></th>');
 		            }else{
-		            	row = row.concat('\n<td class="">',data[i]['codigo'],' ', data[i]['nombre'],'</td>');
-			            row = row.concat('\n<td class="text-center">','----','</td>');
-			            row = row.concat('\n<td class="text-right">$ ',formatNumber(data[i]['Recaudado_2018']),'</td>');
-			            row = row.concat('\n<td class="text-center">','----','</td>');
-			            row = row.concat('\n <td class="text-right">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</td>');
-			            row = row.concat('\n<td class="text-center">',data[i]['var_18_17'],'</td>');
+		            	row = row.concat('\n<td class=""><p class="texto-pequenio">',data[i]['codigo'],' ', data[i]['nombre'],'</p></td>');
+			            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">','----','</p></td>');
+			            row = row.concat('\n<td class="text-right"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2018']),'</p></td>');
+			            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">','----','</p></td>');
+			            row = row.concat('\n <td class="text-right"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</p></td>');
+			            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">',data[i]['var_18_17'],'</p></td>');
 		            }
 		            row = row.concat('\n<tr>');
 		          $("#tbodyReporteResumen").append(row);
@@ -195,19 +195,19 @@
 				            row = row.concat('<tr>');
 				            if(data[i]['nombre'] == 'Total')
 				            {
-				            	row = row.concat('\n<th class="table-active">',data[i]['nombre'],'</th>');
-					            row = row.concat('\n<th class="text-center table-active">','----','</th>');
-					            row = row.concat('\n<th class="text-right table-active">$ ',formatNumber(data[i]['Recaudado_2018']),'</th>');
-					            row = row.concat('\n<th class="text-center table-active">','----','</th>');
-					            row = row.concat('\n<th class="text-right table-active">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</th>');
-					            row = row.concat('\n<th class="text-center table-active">',data[i]['var_18_17'],'</th>');
+				            	row = row.concat('\n<th class="table-active"><p class="texto-pequenio">',data[i]['nombre'],'</p></th>');
+					            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">','----','</p></th>');
+					            row = row.concat('\n<th class="text-right table-active"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2018']),'</p></th>');
+					            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">','----','</p></th>');
+					            row = row.concat('\n<th class="text-right table-active"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</p></th>');
+					            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">',data[i]['var_18_17'],'</p></th>');
 				            }else{
-					            row = row.concat('\n<td class="">',data[i]['codigo'],' ', data[i]['nombre'],'</td>');
-					            row = row.concat('\n<td class="text-center">','----','</td>');
-					            row = row.concat('\n<td class="text-right">$ ',formatNumber(data[i]['Recaudado_2018']),'</td>');
-					            row = row.concat('\n<td class="text-center">','----','</td>');
-					            row = row.concat('\n <td class="text-right">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</td>');
-					            row = row.concat('\n<td class="text-center">',data[i]['var_18_17'],'</td>');
+					            row = row.concat('\n<td class=""><p class="texto-pequenio">',data[i]['codigo'],' ', data[i]['nombre'],'</p></td>');
+					            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">','----','</td>');
+					            row = row.concat('\n<td class="text-right"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2018']),'</p></td>');
+					            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">','----','</p></td>');
+					            row = row.concat('\n <td class="text-right"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</p></td>');
+					            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">',data[i]['var_18_17'],'</p></td>');
 				        	}
 				            row = row.concat('\n</tr>');
 				          $("#tbodyReporteResumenGasto").append(row);
@@ -245,7 +245,7 @@
 	        if (data)
 	        {			
 				$("#hospitales").empty();
-				var row = '<option value="-1">Seleccione un Hospital</option>';
+				var row = '<option value="-1">Todos</option>';
 				for (var i = 0; i < data.length; i++) {
 					row = row.concat('\n<option value="',data[i]["id_hospital"],'">',data[i]["nombre"], '</option>');
 				}
@@ -269,19 +269,19 @@
 		            row = row.concat('<tr>');
 		            if(data[i]['nombre'] == 'Total')
 		            {
-		            	row = row.concat('\n<th class="table-active">',data[i]['nombre'],'</th>');
-			            row = row.concat('\n<th class="text-center table-active">','----','</th>');
-			            row = row.concat('\n<th class="text-right table-active">$ ',formatNumber(data[i]['Recaudado_2018']),'</th>');
-			            row = row.concat('\n<th class="text-center table-active">','----','</th>');
-			            row = row.concat('\n<th class="text-right table-active">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</th>');
-			            row = row.concat('\n<th class="text-center table-active">',data[i]['var_18_17'],'</th>');
+		            	row = row.concat('\n<th class="table-active"><p class="texto-pequenio">',data[i]['nombre'],'</p></th>');
+			            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">','----','</p></th>');
+			            row = row.concat('\n<th class="text-right table-active"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2018']),'</p></th>');
+			            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">','----','</th>');
+			            row = row.concat('\n<th class="text-right table-active"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</p></th>');
+			            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">',data[i]['var_18_17'],'</p></th>');
 		            }else{
-		            	row = row.concat('\n<td class="">',data[i]['codigo'],' ', data[i]['nombre'],'</td>');
-			            row = row.concat('\n<td class="text-center">','----','</td>');
-			            row = row.concat('\n<td class="text-right">$ ',formatNumber(data[i]['Recaudado_2018']),'</td>');
-			            row = row.concat('\n<td class="text-center">','----','</td>');
-			            row = row.concat('\n <td class="text-right">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</td>');
-			            row = row.concat('\n<td class="text-center">',data[i]['var_18_17'],'</td>');
+		            	row = row.concat('\n<td class=""><p class="texto-pequenio">',data[i]['codigo'],' ', data[i]['nombre'],'</p></td>');
+			            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">','----','</td>');
+			            row = row.concat('\n<td class="text-right"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2018']),'</p></td>');
+			            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">','----','</p></td>');
+			            row = row.concat('\n <td class="text-right"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</p></td>');
+			            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">',data[i]['var_18_17'],'</p></td>');
 		            }
 		            row = row.concat('\n<tr>');
 		          $("#tbodyReporteResumen").append(row);
@@ -308,19 +308,19 @@
 				            row = row.concat('<tr>');
 				            if(data[i]['nombre'] == 'Total')
 				            {
-				            	row = row.concat('\n<th class="table-active">',data[i]['nombre'],'</th>');
-					            row = row.concat('\n<th class="text-center table-active">','----','</th>');
-					            row = row.concat('\n<th class="text-right table-active">$ ',formatNumber(data[i]['Recaudado_2018']),'</th>');
-					            row = row.concat('\n<th class="text-center table-active">','----','</th>');
-					            row = row.concat('\n<th class="text-right table-active">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</th>');
-					            row = row.concat('\n<th class="text-center table-active">',data[i]['var_18_17'],'</th>');
+				            	row = row.concat('\n<th class="table-active"><p class="texto-pequenio">',data[i]['nombre'],'</p></th>');
+					            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">','----','</p></th>');
+					            row = row.concat('\n<th class="text-right table-active"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2018']),'</p></th>');
+					            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">','----','</p></th>');
+					            row = row.concat('\n<th class="text-right table-active"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</p></th>');
+					            row = row.concat('\n<th class="text-center table-active"><p class="texto-pequenio">',data[i]['var_18_17'],'</p></th>');
 				            }else{
-					            row = row.concat('\n<td class="">',data[i]['codigo'],' ', data[i]['nombre'],'</td>');
-					            row = row.concat('\n<td class="text-center">','----','</td>');
-					            row = row.concat('\n<td class="text-right">$ ',formatNumber(data[i]['Recaudado_2018']),'</td>');
-					            row = row.concat('\n<td class="text-center">','----','</td>');
-					            row = row.concat('\n <td class="text-right">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</td>');
-					            row = row.concat('\n<td class="text-center">',data[i]['var_18_17'],'</td>');
+					            row = row.concat('\n<td class=""><p class="texto-pequenio">',data[i]['codigo'],' ', data[i]['nombre'],'</p></td>');
+					            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">','----','</p></td>');
+					            row = row.concat('\n<td class="text-right"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2018']),'</p></td>');
+					            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">','----','</p></td>');
+					            row = row.concat('\n <td class="text-right"><p class="texto-pequenio">$ ',formatNumber(data[i]['Recaudado_2017_con_mult']),'</p></td>');
+					            row = row.concat('\n<td class="text-center"><p class="texto-pequenio">',data[i]['var_18_17'],'</p></td>');
 				        	}
 				            row = row.concat('\n</tr>');
 				          $("#tbodyReporteResumenGasto").append(row);
