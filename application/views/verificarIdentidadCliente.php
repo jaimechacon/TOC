82e28a8dd3e61db676cd8c6bd8bc6cb4f6346797
Chacon <jaimechaconarevalo@gmail.com>
2019-03-01 00:32:48 -0300

    se publica validacion de cliente

--- application/views/verificarIdentidadCliente.php
+++ application/views/verificarIdentidadCliente.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Web application for facial recognition with Toc Biometrics API">
+    <meta name="description" content="Web application for facial recognition with GSBPO Biometrics API">
     <meta name="author" content="GSBPO">
     <title data-localize="title">Provida - Verificaci&oacute;n de Identidad</title>
     <!-- Bootstrap core CSS -->
